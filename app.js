cb5658b771f26493cb3d77a07fffc5c6c92d7c80
Aldair47x <aldair47x@gmail.com>
2018-01-25 23:58:12 -0500

    starting the enviroment

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var cors = require('cors');
 var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+var Bing = require('node-bing-api')({accKey:'c12dc7a598e24fa19d19257794b066db'});
+var mongoose = require('mongoose');
+var url = process.env.MONGOLAB_URI;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
-app.use('/users', users);
+
+
+mongoose.connect('mongodb://heroku_qg2qch4m:70u26trgp2t7e64c8gln8td4rb@ds263707.mlab.com:63707/heroku_qg2qch4m' || 'mongodb://localhost/imageSearch');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
